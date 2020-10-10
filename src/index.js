5c2bb0878ad046f19908e9c89a0c1977016fda96
iqoffee <iqoffee@users.noreply.github.com>
2020-10-10 10:52:40 +0500

    feat: reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let p = String(n);
+    if (p[0] === "-") {
+        p = p.slice(1);
+    }
+    p2 = p.split("").reverse().join("");
+    return Number(p2);
+};
